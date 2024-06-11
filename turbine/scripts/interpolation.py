4b6af1da398c486d03c36d29d794ec5fc170bd8e
Victrobv <ictbasv@gmail.com>
2024-06-10 22:24:15 -0400

    fix: fixed error in MultiInterp logic where 1-column DataFrames were not considered for 1D interpolation

--- turbine/scripts/interpolation.py
+++ turbine/scripts/interpolation.py
@@ -16,19 +16,21 @@ def MultiInterp(df, x):
     f: The interpolated value(s) corresponding to the input points.
     """
     
-    if isinstance(df, pd.Series):
-        if np.ndim(x) == 1 and is_numeric_dtype(df.index):
-            xp = df.index.values
-            f = interpn((xp,), df.values, x)
+    if isinstance(df, pd.DataFrame) or isinstance(df, pd.Series):
+        if df.shape[1] == 1:
+            if np.ndim(x) == 1 and is_numeric_dtype(df.index):
+                xp = df.index.values
+                f = interpn((xp,), df.values, x)
+            else:
+                raise ValueError('x must be a 1D array with numeric indexes for 1D interpolation.')
+            
         else:
-            raise ValueError('x must be a 1D array with numeric indexes for 1D interpolation.')
-    elif isinstance(df, pd.DataFrame):
-        if np.ndim(x) == 2 and x.shape[1] == 2 and is_numeric_dtype(df.index) and is_numeric_dtype(df.columns):
-            xp = df.index.values
-            yp = df.columns.values
-            f = interpn((xp, yp), df.values, x)
-        else:
-            raise ValueError('x must be a 2D array of 2 columns with numeric indexes and columns for 2D interpolation.')
+            if np.ndim(x) == 2 and x.shape[1] == 2 and is_numeric_dtype(df.index) and is_numeric_dtype(df.columns):
+                xp = df.index.values
+                yp = df.columns.values
+                f = interpn((xp, yp), df.values, x)
+            else:
+                raise ValueError('x must be a 2D array of 2 columns with numeric indexes and columns for 2D interpolation.')   
     else:
         raise ValueError('df must be a pandas DataFrame or Series')
     return f
